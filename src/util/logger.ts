ac7bd52aa21439e9c1ca91827a2be0e0113ecc01
Ken Vilar <kenvilar@gmail.com>
2021-07-28 11:02:31 +0800

    Remove console logs

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,17 +1,17 @@
 export class Logger {
     public static error(...args: any[]): void {
         // eslint-disable-next-line no-console
-        console.error('[Chonky runtime error]', ...args);
+        // 'kv-custom-chonky' console.error('[Chonky runtime error]', ...args);
     }
 
     public static warn(...args: any[]): void {
         // eslint-disable-next-line no-console
-        console.warn('[Chonky runtime warning]', ...args);
+        // 'kv-custom-chonky' console.warn('[Chonky runtime warning]', ...args);
     }
 
     public static debug(...args: any[]): void {
         // eslint-disable-next-line no-console
-        console.debug('[Chonky runtime debug]', ...args);
+        // 'kv-custom-chonky' console.debug('[Chonky runtime debug]', ...args);
     }
 
     public static formatBullets(bullets: string[]): string {
